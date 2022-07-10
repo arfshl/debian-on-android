c33dc08785b5c19c0ebca9f9dc5200f505466146
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-10 20:29:00 +0700

    Create lxde.sh

---
+++ lxde.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# This script is modification of https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT/LXDE/lxde_de.sh (Licensed under MIT)
+echo 'Installing LXDE, please wait...'
+apt-get install lxde-core lxterminal tigervnc-standalone-server -y
+apt-get clean
+echo 'Setting up LXQt, please wait...'
+mkdir ~/.vnc
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-start -P /usr/local/bin/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-stop -P /usr/local/bin/
+wget https://raw.githubusercontent.com/arfshl/debianonandroid/main/restart -P /usr/local/bin
+cd /usr/local/bin
+mv vncserver-start start
+mv vncserver-stop stop
+chmod +x start
+chmod +x stop
+chmod +x restart
+cd
+chmod +x ~/.vnc/xstartup
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+echo 'Starting up VNC Server'
+echo 'To start VNC server use start command'
+echo 'To stop VNC server use stop command'
+echo 'To restart VNC server use restart command'
+start
