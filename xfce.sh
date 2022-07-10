6c107116b0243ddb8d18d5301238c3c12e4224ab
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-10 11:04:20 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # This script is modification of https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT/XFCE4/xfce4_de.sh (Licensed under MIT)
 echo 'Installing XFCE, please wait...'
-apt-get update
 apt-get install xfce4 xfce4-terminal tigervnc-standalone-server dbus-x11 -y
 apt-get clean
 echo 'Setting up XFCE, please wait...'
