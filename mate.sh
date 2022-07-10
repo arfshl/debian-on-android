0cb1e6ba4b0bbccee92da9d6a769b7ecfd774bfb
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-10 11:03:31 +0700

    Create mate.sh

---
+++ mate.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo 'Installing MATE, please wait...'
+apt-get update
+apt-get install mate-desktop-environment-core mate-terminal tigervnc-standalone-server dbus-x11 -y
+apt-get clean
+echo 'Setting up MATE, please wait...'
+mkdir ~/.vnc
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/vncserver-stop -P /usr/local/bin/
+wget https://raw.githubusercontent.com/arfshl/debianonandroid/main/restart -P /usr/local/bin
+cd /usr/local/bin
+mv vncserver-start start
+mv vncserver-stop stop
+chmod +x start
+chmod +x stop
+chmod +x restart
+cd
+chmod +x ~/.vnc/xstartup
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+echo 'Starting up VNC Server'
+echo 'To start VNC server use start command'
+echo 'To stop VNC server use stop command'
+echo 'To restart VNC server use restart command'
+start
