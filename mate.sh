38bc3564d6750eccda994628b46ac03ae35c0345
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-11 08:34:58 +0700

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-# This script is modification of https://raw.githubusercontent.com/EXALAB/Anlinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/de-apt-mate.sh (Licensed under GPL-2.0)
+# This script is modification of https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT/MATE/mate_de.sh (Licensed under GPL-2.0)
 echo 'Installing MATE, please wait...'
 apt-get install mate-desktop-environment-core tigervnc-standalone-server dbus-x11 -y
 apt-get clean
 echo 'Setting up MATE, please wait...'
 mkdir ~/.vnc
-wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT/MATE/vncserver-stop -P ~/.vnc/
 wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
-wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/vncserver-stop -P /usr/local/bin/
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT/MATE/xstartup -P /usr/local/bin/
 wget https://raw.githubusercontent.com/arfshl/debianonandroid/main/restart -P /usr/local/bin
 cd /usr/local/bin
 mv vncserver-start start
