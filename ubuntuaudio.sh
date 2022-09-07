776ec49ae19883e4a761fd6dc9d4b5a43c84e9eb
arfshl <98870352+arfshl@users.noreply.github.com>
2022-09-07 18:34:49 +0700

    Delete ubuntuaudio.sh

--- ubuntuaudio.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
-clear
-echo 'To start Ubuntu use ubuntu command'
-proot-distro login ubuntu
