aa6bb8058d7ccfb94f4ffddc4bf1e28ff671f699
arfshl <98870352+arfshl@users.noreply.github.com>
2022-10-27 15:50:31 +0700

    Update kde.sh

--- kde/kde.sh
+++ kde/kde.sh
@@ -11,13 +11,47 @@ echo 'Done'
 apt-get clean
 echo 'Setting up KDE, please wait...'
 mkdir ~/.vnc
-curl -o ~/.vnc/xstartup https://raw.githubusercontent.com/EXALAB/Anlinux-Resources/master/Scripts/DesktopEnvironment/Heavy/KDE/Ubuntu/xstartup
-curl -o /usr/local/bin/startkde https://raw.githubusercontent.com/arfshl/debian-on-android/main/kde/startkde
-curl -o /usr/local/bin/stopkde https://raw.githubusercontent.com/arfshl/debian-on-android/main/kde/stopkde
-curl -o /usr/local/bin/restart https://raw.githubusercontent.com/arfshl/debian-on-android/main/restart
+
+echo '#!/bin/bash
+xrdb $HOME/.Xresources
+dbus-launch startplasma-x11' >> ~/.vnc/xstartup
+
+echo '#!/bin/sh
+export USER=root
+export HOME=/root
+
+case `uname -m` in
+	aarch64)
+		archurl="aarch64" ;;
+	arm*)
+		archurl="arm" ;;
+	amd64)
+		archurl="x86_64" ;;
+	x86_64)
+		archurl="x86_64" ;;	
+	i*86)
+		archurl="i386" ;;
+	x86)
+		archurl="i386" ;;
+	*)
+		echo "unknown architecture"; exit 1 ;;
+	esac
+
+LD_PRELOAD=/lib/${archurl}-linux-gnu/libgcc_s.so.1 vncserver -name remote-desktop -localhost no :1' >> /usr/local/bin/start
+
+echo '#!/bin/sh
+export USER=root
+export HOME=/root
+
+vncserver -kill :1
+rm -rf /tmp/.X1-lock
+rm -rf /tmp/.X11-unix/X1' >> /usr/local/bin/stop
+
+echo '#!/bin/sh
+stop
+start' >> /usr/local/bin/restart
+
 cd /usr/local/bin
-mv startkde start
-mv stopkde stop
 chmod +x start
 chmod +x stop
 chmod +x restart
@@ -28,7 +62,7 @@ source /etc/profile
 apt remove konqueror ksysguard plasma-discover partitionmanager kdeconnect termit kwalletmanager -y
 apt autoremove -y
 passwd
-echo 'Setting up Pulseaudio'
+echo 'Setting up Pulseaudio...'
 echo 'export PULSE_SERVER=127.0.0.1' >> ~/.bashrc
 echo 'Starting up VNC Server'
 echo 'To start VNC server use start command'
