23d499d199455eb120db2dd501f155d8143186f6
arfshl <98870352+arfshl@users.noreply.github.com>
2022-10-27 15:39:14 +0700

    Update and rename mate/mate.sh to mate.sh

--- mate.sh
+++ mate.sh
@@ -5,10 +5,32 @@ apt-get install mate-desktop-environment-core tigervnc-standalone-server dbus-x1
 apt-get clean
 echo 'Setting up MATE, please wait...'
 mkdir ~/.vnc
-curl -o ~/.vnc/xstartup https://raw.githubusercontent.com/arfshl/debian-on-android/main/mate/xstartup
-curl -o /usr/local/bin/vncserver-start https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start 
-curl -o /usr/local/bin/vncserver-stop https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT/MATE/vncserver-stop 
-curl -o /usr/local/bin/restart https://raw.githubusercontent.com/arfshl/debian-on-android/main/restart
+
+echo '#!/bin/sh
+xrdb $HOME/.Xresources
+mate-session &' >> ~/.vnc/xstartup
+
+echo '#!/usr/bin/env bash
+
+export USER=root
+export HOME=/root
+
+vncserver -kill :1
+rm -rf /root/.vnc/localhost:1.pid
+rm -rf /tmp/.X1-lock
+rm -rf /tmp/.X11-unix/X1' >> /usr/local/bin/stop
+
+echo '#!/usr/bin/env bash
+
+export USER=root
+export HOME=/root
+
+vncserver -name remote-desktop -localhost no :1' >> /usr/local/bin/start
+
+echo '#!/bin/sh
+stop
+start' >> /usr/local/bin/restart
+
 cd /usr/local/bin
 mv vncserver-start start
 mv vncserver-stop stop
@@ -21,7 +43,7 @@ echo "export DISPLAY=":1"" >> /etc/profile
 source /etc/profile
 apt remove xterm -y
 apt autoremove -y
-echo 'Setting up Pulseaudio'
+echo 'Setting up Pulseaudio...'
 echo 'export PULSE_SERVER=127.0.0.1' >> ~/.bashrc
 echo 'Starting up VNC Server'
 echo 'To start VNC server use start command'
