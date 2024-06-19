9e8d6bff996adae8783621d69328f2f3cec8f38e
arfshl <98870352+arfshl@users.noreply.github.com>
2024-06-19 04:57:30 +0000

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # This script is modification of https://github.com/AndronixApp/AndronixOrigin/raw/master/WM/APT/i3.sh (Licensed under MIT)
 echo "Installing i3..."
-apt install keyboard-configuration dialog libexo-2-0 i3 tigervnc-standalone-server nano dbus-x11 xorg xfce4-terminal thunar cairo-dock tigervnc-common --no-install-recommends -y
-apt-get clean
+apt install sudo
+sudo apt install keyboard-configuration dialog libexo-2-0 i3 tigervnc-standalone-server nano dbus-x11 xorg xfce4-terminal thunar cairo-dock tigervnc-common --no-install-recommends -y
+sudo apt-get clean
 echo "Setting up i3..."
 mkdir ~/.vnc
 
@@ -13,15 +14,15 @@ export ~/.Xauthority
 dbus-launch i3
 dbus-launch cairo-dock" > ~/.vnc/xstartup
 
-echo '#!/usr/bin/env bash
+sudo echo '#!/usr/bin/env bash
 
 export USER=root
 export HOME=/root
 
 vncserver -name remote-desktop -localhost no :1
-echo 'VNC server address: 127.0.0.1:1'' >> /usr/local/bin/start 
+sudo echo 'VNC server address: 127.0.0.1:1'' >> /usr/local/bin/start 
 
-echo '#!/usr/bin/env bash
+sudo echo '#!/usr/bin/env bash
 
 export USER=root
 export HOME=/root
@@ -31,20 +32,20 @@ rm -rf /root/.vnc/localhost:1.pid
 rm -rf /tmp/.X1-lock
 rm -rf /tmp/.X11-unix/X1' >> /usr/local/bin/stop
 
-echo '#!/bin/sh
+sudo echo '#!/bin/sh
 stop
 start' >> /usr/local/bin/restart
 
 cd /usr/local/bin
-chmod +x start
-chmod +x stop
-chmod +x restart
+sudo chmod +x start
+sudo chmod +x stop
+sudo chmod +x restart
 cd
 chmod +x ~/.vnc/xstartup
-echo "export DISPLAY=":1"" >> /etc/profile
+sudo echo "export DISPLAY=":1"" >> /etc/profile
 source /etc/profile
-apt remove xterm -y
-apt autoremove -y
+sudo apt remove xterm -y
+sudo apt autoremove -y
 echo 'Starting up VNC Server'
 echo 'To start VNC server use start command'
 echo 'To stop VNC server use stop command'
