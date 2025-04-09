ce48e0fc036c41dc90637dab483f2d78ce65983d
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2025-04-09 20:47:11 +0700

    Update openbox.sh

--- openbox.sh
+++ openbox.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This script is modification of https://github.com/AndronixApp/AndronixOrigin/raw/master/WM/APT/openbox.sh (Licensed under MIT)
 echo "Installing Openbox..."
-apt install keyboard-configuration tzdata dialog libexo-2-0 openbox obconf tigervnc-standalone-server nano dbus-x11 xorg xfce4-terminal thunar cairo-dock --no-install-recommends -y
+apt install sudo addusr
+sudo apt install keyboard-configuration tzdata dialog libexo-2-0 openbox obconf tigervnc-standalone-server nano dbus-x11 xorg xfce4-terminal thunar cairo-dock pulseaudio --no-install-recommends -y
 apt-get clean
 echo 'Setting up Openbox...'
 mkdir -p ~/.vnc
