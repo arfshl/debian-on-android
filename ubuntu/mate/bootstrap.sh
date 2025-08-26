0137c2bed39db9163c47b656ee297f34eab61973
Alif <arfshl@protonmail.com>
2025-08-26 14:42:51 +0700

    Update bootstrap.sh

--- ubuntu/mate/bootstrap.sh
+++ ubuntu/mate/bootstrap.sh
@@ -20,6 +20,8 @@ proot-distro login ubuntu-mate --user ubuntu-mate' >> /data/data/com.termux/file
 # for X11 session
 cat <<EOF > /data/data/com.termux/files/usr/bin/startubuntu-mate-x11
 #!/bin/sh
+LD_PRELOAD=/system/lib64/libskcodec.so
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 export XDG_RUNTIME_DIR=${TMPDIR}
 kill -9 \$(pgrep -f "termux.x11")\ 2>/dev/null
 kill -9 \$(pgrep -f "virgl")\ 2>/dev/null
