b70d7c8d515a01f448624e5a829b50a130afd1d0
Alif <arfshl@protonmail.com>
2025-08-26 14:40:24 +0700

    Update bootstrap.sh

--- ubuntu/kde/bootstrap.sh
+++ ubuntu/kde/bootstrap.sh
@@ -11,17 +11,12 @@ apt upgrade -y -o Dpkg::Options::="--force-confold"
 # Install depedency
 apt install curl wget nano proot-distro termux-x11 pulseaudio vulkan-loader-android mesa-zink virglrenderer-mesa-zink virglrenderer-android -y
 
-# PulseAudio at Setup
+# for CLI session
+echo '#!/bin/sh
 LD_PRELOAD=/system/lib64/libskcodec.so
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+proot-distro login ubuntu-kde --user ubuntu-kde' >> /data/data/com.termux/files/usr/bin/startubuntu-xfce
 
-# PulseAudio at startup on bash.bashrc
-echo 'LD_PRELOAD=/system/lib64/libskcodec.so
-pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> $PREFIX/etc/bash.bashrc
-
-# Create startup script
-# for CLI session
-printf 'proot-distro login ubuntu-kde --user ubuntu-kde' >> /data/data/com.termux/files/usr/bin/startubuntu-kde
 
 # for X11 session
 cat <<EOF > /data/data/com.termux/files/usr/bin/startubuntu-kde-x11
