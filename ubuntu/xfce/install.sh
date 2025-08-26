605988fe0c0c02adf677a354b0deeb7c3d1fc55c
Alif <arfshl@protonmail.com>
2025-08-26 14:33:30 +0700

    Update install.sh

--- ubuntu/xfce/install.sh
+++ ubuntu/xfce/install.sh
@@ -59,14 +59,6 @@ chmod +x restartvnc
 cd
 chmod +x /home/ubuntu-xfce/.vnc/xstartup
 
-# Install Apps
-apt update && apt install chromium firefox vlc thunderbird atril ristretto galculator libheif1 xarchiver webp-pixbuf-loader fonts-liberation p7zip mousepad -y && apt clean
-sudo sed -i 's|chromium --incognito %U|chromium --incognito --no-sandbox %U|' /usr/share/applications/chromium.desktop
-sudo sed -i 's|chromium %U|chromium --no-sandbox %U|' /usr/share/applications/chromium.desktop
-sudo sed -i 's|chromium --new-window %U|chromium --new-window --no-sandbox %U|' /usr/share/applications/chromium.desktop
-sudo rm /etc/chromium/master_preferences
-wget https://raw.githubusercontent.com/arfshl/proot-distro-desktop/refs/heads/main/scripts/office-online.desktop -P /usr/share/applications/
-
 
 
 
