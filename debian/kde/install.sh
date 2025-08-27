1bd92783d551d503c208fa147771dd241cf128fa
Alif <arfshl@protonmail.com>
2025-08-27 19:42:17 +0700

    Update install.sh

--- debian/kde/install.sh
+++ debian/kde/install.sh
@@ -51,7 +51,7 @@ export HOME=/home/debian-kde
 vncserver -kill :0
 rm -rf /home/debian-kde/.vnc/localhost:0.pid
 rm -rf /tmp/.X0-lock
-rm -rf /tmp/.X11-unix/X0>> /usr/local/bin/stopvnc
+rm -rf /tmp/.X11-unix/X0' >> /usr/local/bin/stopvnc
 
 # Create script for restarting VNC server
 echo '#!/bin/sh
