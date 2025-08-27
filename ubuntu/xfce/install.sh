53d014521c3c136219c04be6bb68f96cdb1c9101
Alif <arfshl@protonmail.com>
2025-08-27 19:19:48 +0700

    Update install.sh

--- ubuntu/xfce/install.sh
+++ ubuntu/xfce/install.sh
@@ -66,7 +66,7 @@ export HOME=/home/ubuntu-xfce
 vncserver -kill :0
 rm -rf /home/ubuntu-xfce/.vnc/localhost:0.pid
 rm -rf /tmp/.X0-lock
-rm -rf /tmp/.X11-unix/X0>> /usr/local/bin/stopvnc
+rm -rf /tmp/.X11-unix/X0' >> /usr/local/bin/stopvnc
 
 # Create script for restarting VNC server
 echo '#!/bin/sh
