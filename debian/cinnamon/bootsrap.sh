dd06ab282d1214265de400743e01ef3dd1c7e593
Alif <arfshl@protonmail.com>
2025-08-27 19:44:54 +0700

    Update bootsrap.sh

--- debian/cinnamon/bootsrap.sh
+++ debian/cinnamon/bootsrap.sh
@@ -18,12 +18,12 @@ pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth
 proot-distro login debian-cinnamon --user debian-cinnamon' >> /data/data/com.termux/files/usr/bin/debian-cinnamon
 
 # for X11 session
-cat <<EOF > /data/data/com.termux/files/usr/bin/debian-cinnamon-x11
+cat <<'EOF' > /data/data/com.termux/files/usr/bin/debian-cinnamon-x11
 #!/bin/sh
 LD_PRELOAD=/system/lib64/libskcodec.so
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 export XDG_RUNTIME_DIR=${TMPDIR}
-kill -9 \$(pgrep -f "termux.x11")\ 2>/dev/null
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 proot-distro login debian-cinnamon --shared-tmp -- /bin/sh -c 'kill -9 $(pgrep -f "x11") 2>/dev/null'
 termux-x11 :0 >/dev/null &
 sleep 3
@@ -37,6 +37,8 @@ chmod +x /data/data/com.termux/files/usr/bin/debian-cinnamon*
 proot-distro install debian --override-alias debian-cinnamon
 
 # Setup debian-cinnamon
+proot-distro login debian-cinnamon -- /bin/sh -c 'apt update && apt install wget -y'
+
 proot-distro login debian-cinnamon -- /bin/sh -c 'wget https://raw.githubusercontent.com/arfshl/proot-distro-desktop/refs/heads/main/debian/cinnamon/install.sh -O install.sh && chmod +x install.sh && ./install.sh && rm install.sh'
 
 echo 'To start command line session: debian-cinnamon'
