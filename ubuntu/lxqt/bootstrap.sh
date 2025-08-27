41565fd747256066c4d46bc9bb5e648754a75ead
Alif <arfshl@protonmail.com>
2025-08-27 19:25:55 +0700

    Update bootstrap.sh

--- ubuntu/lxqt/bootstrap.sh
+++ ubuntu/lxqt/bootstrap.sh
@@ -23,8 +23,8 @@ cat <<EOF > /data/data/com.termux/files/usr/bin/ubuntu-lxqt-x11
 LD_PRELOAD=/system/lib64/libskcodec.so
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 export XDG_RUNTIME_DIR=${TMPDIR}
-kill -9 \$(pgrep -f "termux.x11")\ 2>/dev/null
-kill -9 \$(pgrep -f "virgl")\ 2>/dev/null
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+kill -9 $(pgrep -f "virgl") 2>/dev/null
 proot-distro login ubuntu-lxqt --shared-tmp -- /bin/sh -c 'kill -9 $(pgrep -f "x11") 2>/dev/null'
 virgl_test_server_android &
 termux-x11 :0 >/dev/null &
