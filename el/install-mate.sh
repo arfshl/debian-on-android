1387b9916376641f39b34534d9f379d1cfa4381c
Alif <arfshl@protonmail.com>
2025-09-01 19:27:30 +0700

    Update install-mate.sh

--- el/install-mate.sh
+++ el/install-mate.sh
@@ -5,36 +5,36 @@ dnf install --nogpgcheck -y https://mirrors.rpmfusion.org/free/el/rpmfusion-free
 dnf install -y @base-x mate-desktop mate-applets mate-backgrounds mate-control-center mate-media mate-menu mate-menus mate-notification mate-panel mate-icon-theme mate-power-manager mate-search-tool mate-terminal mate-themes mate-utils caja wget curl sudo nano pulseaudio pavucontrol xdg-user-dirs tigervnc rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
 
 # Adding user and password
-useradd -m el-xfce && echo 'el-xfce:123' | chpasswd && echo 'el-xfce ALL=(ALL:ALL) ALL' >> /etc/sudoers.d/user
+useradd -m el-mate && echo 'el-mate:123' | chpasswd && echo 'el-mate ALL=(ALL:ALL) ALL' >> /etc/sudoers.d/user
 
 # Setup VNC server
 # Create VNC configuration directory
-mkdir -p /home/el-xfce/.vnc
+mkdir -p /home/el-mate/.vnc
 
 # Create VNC password file (default 1234567890)
-printf "1234567890" | vncpasswd -f > /home/el-xfce/.vnc/passwd
-chmod 600 /home/el-xfce/.vnc/passwd
+printf "1234567890" | vncpasswd -f > /home/el-mate/.vnc/passwd
+chmod 600 /home/el-mate/.vnc/passwd
 
 # Create VNC startup script
 echo '#!/bin/sh
 xrdb $HOME/.Xresources
 export PULSE_SERVER=127.0.0.1
 export DISPLAY=:0
-dbus-launch --exit-with-session mate-session' >> /home/el-xfce/.vnc/xstartup
+dbus-launch --exit-with-session mate-session' >> /home/el-mate/.vnc/xstartup
 
 # Create script for starting VNC server
 echo "#!/bin/sh
-export USER=el-xfce
-export HOME=/home/el-xfce
+export USER=el-mate
+export HOME=/home/el-mate
 vncserver -name remote-desktop -localhost no :0
 echo 'VNC server address: 127.0.0.1:5900 Password: 1234567890'" >> /usr/local/bin/startvnc
 
 # Create script for stopping VNC server
 echo '#!/bin/sh
-export USER=el-xfce
-export HOME=/home/el-xfce
+export USER=el-mate
+export HOME=/home/el-mate
 vncserver -kill :0
-rm -rf /home/el-xfce/.vnc/localhost:0.pid
+rm -rf /home/el-mate/.vnc/localhost:0.pid
 rm -rf /tmp/.X0-lock
 rm -rf /tmp/.X11-unix/X0' >> /usr/local/bin/stopvnc
 
@@ -49,4 +49,4 @@ chmod +x startvnc
 chmod +x stopvnc
 chmod +x restartvnc
 cd
-chmod +x /home/el-xfce/.vnc/xstartup
+chmod +x /home/el-mate/.vnc/xstartup
