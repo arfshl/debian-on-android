fe7f7616f6dd388b85eeab91b352e9e0459096ab
Alif <arfshl@protonmail.com>
2025-09-06 11:45:45 +0700

    Create remove-full.sh

---
+++ opensuse/tumbleweed/cinnamon/remove-full.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+proot-distro remove tumbleweed-cinnamon
+
+rm /data/data/com.termux/files/usr/bin/tumbleweed-cinnamon
+rm /data/data/com.termux/files/usr/bin/tumbleweed-cinnamon-x11
+
+apt remove proot-distro termux-x11 pulseaudio vulkan-loader-android mesa-zink virglrenderer-mesa-zink virglrenderer-android -y && apt autoremove -y
+
+apt remove x11-repo tur-repo -y && apt update
