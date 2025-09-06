ccf5941456c11cb5c37143e0e5f8a7211ba57074
Alif <arfshl@protonmail.com>
2025-09-06 11:11:36 +0700

    Update remove-rootfs.sh

--- opensuse/tumbleweed/lxqt/remove-rootfs.sh
+++ opensuse/tumbleweed/lxqt/remove-rootfs.sh
@@ -4,6 +4,3 @@ proot-distro remove tumbleweed-lxqt
 rm /data/data/com.termux/files/usr/bin/tumbleweed-lxqt
 rm /data/data/com.termux/files/usr/bin/tumbleweed-lxqt-x11
 
-apt remove proot-distro termux-x11 pulseaudio vulkan-loader-android mesa-zink virglrenderer-mesa-zink virglrenderer-android -y && apt autoremove -y
-
-apt remove x11-repo tur-repo -y && apt update
