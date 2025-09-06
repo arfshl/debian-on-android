dd5dd7ca868f4bd8d79f0ea636a4bc094c66e34f
Alif <arfshl@protonmail.com>
2025-09-06 11:07:27 +0700

    Update bootstrap.sh

--- opensuse/tumbleweed/lxde/bootstrap.sh
+++ opensuse/tumbleweed/lxde/bootstrap.sh
@@ -30,7 +30,7 @@ proot-distro login tumbleweed-lxde --shared-tmp -- /bin/sh -c 'kill -9 $(pgrep -
 virgl_test_server_android &
 termux-x11 :0 >/dev/null &
 sleep 3
-proot-distro login tumbleweed-lxde --shared-tmp -- /bin/sh -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - opensuse-lxde -c "DISPLAY=:0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session startxfce4"'
+proot-distro login tumbleweed-lxde --shared-tmp -- /bin/sh -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - opensuse-lxde -c "DISPLAY=:0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session startlxde"'
 EOF
 
 # Make all of them executable
